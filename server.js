5b3646bcda19dec40b0d590c5820b4ba3aae9cfa
Helder Calado <hbcalado@gmail.com>
2018-12-18 09:37:32 -0800

    second commit

--- server.js
+++ server.js
@@ -15,4 +15,9 @@ app.use('/api', api);
 
 
 
-app.listen(3000);
+app.set( 'port', ( process.env.PORT || 5000 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
